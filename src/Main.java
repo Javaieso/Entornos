3a80e3159ee6d0dc6084290745db2dffa7700343
Javier <https://github.com/Javaieso/Entornos.git>
2026-04-14 13:53:16 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Mensaje de inicio del proyecto
+        System.out.println("Versión inicial con gestión básica de productos.");
+
+        // Mensaje opcional de confirmación
+        System.out.println("MiniInventario iniciado correctamente.");
+    }
+}
+
